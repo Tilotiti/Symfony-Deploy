24726b6a9c180d522c63d9be8811d5bb8a588d4c
Thibault Henry <thibaulthenry@tilotiti.com>
2016-08-09 18:37:27 +0200

    Add Composer option

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 echo "---- Updating a Symfony 3.x project ----"
 
+# Option "Composer" -c
+composer="false"
+
+while getopts c name
+do
+		case $name in
+			c)
+				composer="true"
+				;;
+		esac
+done
+
 # Reset the git for cancel unstaging modifications
 git reset --hard origin/master
 
 # Get the last code
 git pull
 
-# Update dependencies
-composer update
+# Update dependencies only if -c is "true"
+if [ "$composer" == "true" ]
+then
+	composer update
+fi
 
 # Update database schema
 bin/console doctrine:schema:update --force
